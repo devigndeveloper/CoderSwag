5efef4adfb871f84b43e2af53fdcb6e53d7c82af
devigndeveloper <info@devign.co.nz>
2019-09-22 07:08:25 +1200

    First List created - Title currently clashing with list

--- app/src/main/java/com/devign/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/devign/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,23 @@ package com.devign.coderswag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.devign.coderswag.Model.Category
 import com.devign.coderswag.R
+import com.devign.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            DataService.catergories)
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/com/devign/coderswag/Model/Category.kt
+++ app/src/main/java/com/devign/coderswag/Model/Category.kt
@@ -1,3 +1,9 @@
 package com.devign.coderswag.Model
 
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String) {
+
+    // Force the view to show 'title' as opposed to the entire strings data
+    override fun toString(): String {
+        return title
+    }
+}
