379c5d310f818a97420b626f0eb0b43ecbe8a4b7
devigndeveloper <info@devign.co.nz>
2019-09-26 08:41:59 +1200

    View Holder created and working

--- app/src/main/java/com/devign/coderswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/devign/coderswag/Adapters/CategoryAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.devign.coderswag.Model.Category
 import com.devign.coderswag.R
 import java.text.FieldPosition
@@ -20,19 +21,33 @@ class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapte
     // What a user sees
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val categoryView: View
+        val holder : ViewHolder
 
-        // Pushes out the data
-        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
-        val categoryImage : ImageView = categoryView.findViewById(R.id.categoryImage)
-        val categoryName : TextView = categoryView.findViewById(R.id.categoryName)
+        // if we show the data the first time, do this
+        if (convertView == null) {
+            // Pushes out the data - inflates layout, grabs template
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+            // Initialises the holder - The holder stops the phone from reloading data once scrolled away. It holds onto the data, so that when a user scrolls back to it it can display it - saving space in the memory
+            holder = ViewHolder()
 
+            // Finds the image and name and pushes it out through the holder
+            holder.categoryImage = categoryView.findViewById(R.id.categoryImage)
+            holder.categoryName = categoryView.findViewById(R.id.categoryName)
+
+            categoryView.tag = holder
+        } else {
+            // if the data has been created already, re-use the data we're holding
+            holder = convertView.tag as ViewHolder
+            categoryView = convertView
+        }
+
+
+
+        // for loops the array to give each category an index
         val category = categories[position]
-
         val resourceId = context.resources.getIdentifier(category.image, "drawable",context.packageName)
-        categoryImage.setImageResource(resourceId)
-        println(resourceId)
-
-        categoryName.text = category.title
+        holder.categoryImage?.setImageResource(resourceId)
+        holder.categoryName?.text = category.title
 
         return categoryView
     }
@@ -50,4 +65,11 @@ class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapte
     override fun getCount(): Int {
         return categories.count()
     }
+
+    // Let's us re-use and recycle data
+    private class ViewHolder {
+        var categoryImage: ImageView? = null
+        var categoryName: TextView? = null
+    }
+
 }
